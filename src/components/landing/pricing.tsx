425795df03c184028034710a6b7437d17f251137
Rodrigo M <rm0808086@gmail.com>
2025-09-21 23:40:27 +0000

    Aumente esse botão (_for element <LinkComponent>_)

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -68,7 +68,7 @@ export function Pricing() {
               </ul>
             </CardContent>
             <CardFooter>
-              <Button asChild className="w-full font-bold text-lg transition-transform duration-300 hover:scale-[1.03] hover:shadow-glow !text-white">
+              <Button asChild className="w-full font-bold text-lg transition-transform duration-300 hover:scale-[1.03] hover:shadow-glow !text-white py-6">
                 <Link href="#">Quero o Premium</Link>
               </Button>
             </CardFooter>
