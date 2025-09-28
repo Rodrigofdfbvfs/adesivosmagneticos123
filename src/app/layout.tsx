7208e24b519c34e4ac7c53bd97b5f158b14f6476
Rodrigo M <rm0808086@gmail.com>
2025-09-28 20:47:59 +0000

    Instale esse pixel do facebook no site <!-- Meta Pixel Code --> <script>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,27 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet" />
         <Script src="https://fast.wistia.com/player.js" strategy="beforeInteractive" />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '809479004946925');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        <noscript>
+            <img height="1" width="1" style={{display: 'none'}}
+                 src="https://www.facebook.com/tr?id=809479004946925&ev=PageView&noscript=1"
+            />
+        </noscript>
         <DateBanner />
         {children}
         <Toaster />
