c99888a7f94429bfe5fa87e873a9a5a277c519e5
Rodrigo M <rm0808086@gmail.com>
2025-11-26 11:44:51 +0000

    <script>   window.pixelId = "6926e7ebb1a5e6edc1d7a949";   var a = docume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,13 +56,16 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
-        <script
-          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
-          data-utmify-prevent-xcod-sck
-          data-utmify-prevent-subids
-          async
-          defer
-        ></script>
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "6926e7ebb1a5e6edc1d7a949";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
         <noscript>
